90ab1773d2680da4a5d6a3e4b78d64e7b69151d5
Abebe Gedefaw Tegegne <99889502+zalmat123@users.noreply.github.com>
2022-03-15 16:34:48 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,48 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * print_times_table - Prints a multiplication table up to param
-  * @n: The number to be treated
-  *
-  * Return: Number matrix
-  */
+ * * print_times_table - Print the n times table
+ *
+ * @n: number times table table (0 < n<= 15)
+ *
+ * Return: no return
+ */
+
 void print_times_table(int n)
 {
-	int x, y, z;
+	int a, b, op;
 
-	if (n >= 0 && n <= 14)
+	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; x++)
+	for (a = 0; a <= n; a++)
+	{
+	_putchar(48);
+	for (b = 1; b <= n; b++)
+	{
+		op = a * b;
+		_putchar(44);
+		_putchar(32);
+		if (op <= 9)
 		{
-			for (y = 0; y <= n; y++)
-			{
-				z = x * y;
-				if (z > 99)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else if (z > 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else
-				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
-				}
-			}
-			_putchar('\n');
+		_putchar(32);
+		_putchar(32);
+		_putchar(op + 48);
 		}
+		else if (op <= 99)
+		{
+		_putchar(32);
+		_putchar((op / 10) + 48);
+		_putchar((op % 10) + 48);
+		}
+		else
+		{
+		_putchar(((op / 100) % 10) + 48);
+		_putchar(((op / 10) % 10) + 48);
+		_putchar((op % 10) + 48);
+		}
+	}
+	_putchar('\n');
+	}
 	}
 }
 
