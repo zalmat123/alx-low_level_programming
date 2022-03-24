95d594530b106f7123726819434cb14ea9d3eeb8
Abebe Gedefaw Tegegne <99889502+zalmat123@users.noreply.github.com>
2022-03-24 09:18:22 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -38,4 +38,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			s = 0;
 		r[k] = (f + s + d) % 10 + '0';
-
+		d = (f + s + d) / 10;
+	}
+	if (d == 1)
+	{
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
+	}
+	return (r);
+}
